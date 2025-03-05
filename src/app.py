d891824d64bca4ea16b000297e189b91e07281bf
Vishtyagi <vishal.tyagi@easyjet.com>
2025-03-05 18:01:55 +0530

    fixed linting issues

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
-import psycopg2 ,os
+import psycopg2
+import os
 import logging
 
 app = Flask(__name__)
@@ -7,18 +8,22 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)  
 logger = logging.getLogger(__name__)
 
+
 def get_db_connection():
     try:    
         logger.info("Connecting to the database...")
-        conn = psycopg2.connect(host=os.getenv('DATABASE_HOST'),
-        database=os.getenv('DATABASE_NAME'),
-        user=os.getenv('DATABASE_USER'),
-        password=os.getenv('DATABASE_PASSWORD'),
-        port=5432)
+        conn = psycopg2.connect(
+            host=os.getenv('DATABASE_HOST'),
+            database=os.getenv('DATABASE_NAME'),
+            user=os.getenv('DATABASE_USER'),
+            password=os.getenv('DATABASE_PASSWORD'),
+            port=5432
+        )
         return conn
     except Exception as e:
         logger.error(f"Database connection failed: {e}")
-        return None 
+        return None
+
 
 @app.route("/", methods=["GET", "POST"])
 def form():
@@ -28,7 +33,10 @@ def form():
             email = request.form["email"]
             conn = get_db_connection()
             cursor = conn.cursor()
-            cursor.execute("INSERT INTO users (name, email) VALUES (%s, %s)", (name, email))
+            cursor.execute(
+                "INSERT INTO users (name, email) VALUES (%s, %s)",
+                (name, email)
+            )
             conn.commit()
             cursor.close()
             conn.close()
@@ -38,9 +46,11 @@ def form():
             return "<h1>Something went wrong!</h1>"
     return render_template("form.html")
 
+
 @app.route("/success")
 def success():
     return "<h1>Data saved successfully!</h1>"
 
+
 if __name__ == "__main__":
-     app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
